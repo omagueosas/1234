dbf0c1680340b7596f9df28f4fa950a8bbdc1b32
omagueosas <117897455+omagueosas@users.noreply.github.com>
2024-01-18 01:04:36 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function viewDiv(){
+    document.getElementById("manager").style.display = "block";
+};
+
+function hideDiv(){
+    document.getElementById("manager").style.display = "none";
+};
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+    observer.observe(elm);
+  }
---
+++ assets/js/slider.js
@@ -0,0 +1,15 @@
+$('.single-item').slick(
+    {
+        autoplay: true,
+        arrows : false,
+    });
+ 
+    
+
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    });
+            
